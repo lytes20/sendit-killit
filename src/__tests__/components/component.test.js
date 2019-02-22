0d790d926fd040079fe42a0e64602d8b29184688
anyatibrian <anyatibrian@gmail.com>
2019-02-22 14:37:14 +0300

    [chore #164000826] creates the first test to check whether it passes

--- src/__tests__/components/component.test.js
+++ src/__tests__/components/component.test.js
@@ -1,10 +1,13 @@
-import 'jest-enzyme';
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from '../../components/Home';
 
 // eslint-disable-next-line no-undef
-it('renders without crashing', () => {
+it('renders renders the home component', () => {
   // eslint-disable-next-line react/jsx-filename-extension
-  shallow(<Home />);
+  const wrapper = shallow(<Home />);
+  const text = <h2> the is the home component</h2>;
+  // eslint-disable-next-line no-undef
+  expect(wrapper.contains(text)).toBe(true);
 });
