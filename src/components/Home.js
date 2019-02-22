99356839db94ff49964a61f26a882d4d687b1c60
anyatibrian <anyatibrian@gmail.com>
2019-02-22 14:32:45 +0300

    [chore #164000826] creates the first test to check whether it passes

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <h2> the is the home component</h2>
+            </div>
+        );
+    }
+}
+export default Home;
