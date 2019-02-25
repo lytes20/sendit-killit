4d15e29f60e311d83cb65c92f3199564887fe40a
anyatibrian <anyatibrian@gmail.com>
2019-02-25 16:56:06 +0300

    adds validation massages so that they can be able to get errors incase of any

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {FormValidation} from '../utilities/formValidations';
 
 class Login extends Component {
   constructor(props) {
@@ -10,7 +11,8 @@ class Login extends Component {
       formErrors: {
         username: '',
         password: ''
-      }
+      },
+      errors: ''
     };
   }
   // handling onchange event
@@ -18,16 +20,36 @@ class Login extends Component {
     event.preventDefault();
     const {name, value}= event.target;
     const {formErrors} = this.state;
+    // validation for the form fields
+    switch(name){
+      case 'username':
+        formErrors.username = value.length < 6 && value.length > 0 ? 'username must be atleast six character' : '';
+        break;
+      case 'password':
+        formErrors.password = value.length < 6 && value.length > 0 ? 'password must be atleast six character' : '';
+        break;
+      default:
+      break;
+    }
     this.setState({formErrors, [name]: value });
+    console.log(this.state);
   };
  
   // form on submit method
   handleOnsubmit = event => {
     event.preventDefault();
+    const {username,password, formErrors}= this.state;
+    if(FormValidation(formErrors,username,password)){
+      console.log(this.state);
+    }else{
+      this.setState({errors: 'please enter your username and password'});
+      console.log(this.state.errors);
+    }
   };
 
   render() {
-    const {username,password} = this.state;
+    const {username,password, errors, formErrors} = this.state;
+    
     return (
       <div>
         <div className="row">
@@ -45,6 +67,7 @@ class Login extends Component {
                 value={username}
                 onChange={this.handleonChange}
               />
+                {formErrors.username.length > 0 && (<span className="text text-danger">{formErrors.username}</span>)}
             </div>
             <div className="form-group">
               <label>password</label>
@@ -56,6 +79,10 @@ class Login extends Component {
                 value={password}
                 onChange ={this.handleonChange} 
                 />
+            </div>
+            {formErrors.password.length >0 && (<span className="text text-danger">{formErrors.password}</span>)}
+            <div className="mb-4">
+              <span className="text text-danger">{errors}</span>
             </div>
             <div className="form-group">
             <button className="btn btn-outline-danger"> Login</button>
--- src/utilities/formValidations.js
+++ src/utilities/formValidations.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
-export const FormValidation = (username, password) => {
+export const FormValidation = (formError, username, password) => {
   let valide = true;
+  // validation for each form field during user login
+  Object.values(formError).forEach(val => {
+    val.length > 0 && (valide = false);
+  });
+  // validtion to check where the username field is empty
   if (username === '' && password === '') {
     valide = false;
   }
