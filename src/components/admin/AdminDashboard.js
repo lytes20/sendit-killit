6396e2c7e6fcd91f52e4e8ed19a7587eeb3a1ac6
Daniel Lwetabe <acehanks@gmail.com>
2019-02-28 02:16:26 +0300

    [manually add token from login to dashboard. CORS error not resolved]

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import NavBar from './AppBar';
 
 export class AdminDashboard extends Component {
+  componentDidMount() {
+    //   console.log(this.props);
+    fetch('https://francissendit.herokuapp.com/api/v2/parcels', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        token:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE1NTEzMTE2MjB9.ArFdUHp2KS6FojNmfLApUIHrTB0iRJA1di7kiGi1WDI',
+        Accept: 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+      });
+  }
   render() {
     return (
       <React.Fragment>
--- src/components/admin/AdminLogin.js
+++ src/components/admin/AdminLogin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Redirect } from 'react-router-dom';
 
 class AdminLogin extends Component {
   state = {
@@ -35,12 +36,14 @@ class AdminLogin extends Component {
       .then(res => {
         const loggedIn = res['login_message'];
         const token = loggedIn['token_generated'];
+        console.log(token);
         const role = loggedIn['user_role'];
         this.setState({ _token: token, isLoggedIn: true });
       });
   };
 
   render() {
+    const tKey = this.state._token;
     return (
       <React.Fragment>
         <div className="heading">
@@ -78,11 +81,16 @@ class AdminLogin extends Component {
           </div>
         </div>
 
-        {/* {this.state.isLoggedIn ? (
-          <Redirect to="/admin/dashboard/" />
+        {this.state.isLoggedIn ? (
+          <Redirect
+            to={{
+              pathname: '/dashboard',
+              state: { id: '123' }
+            }}
+          />
         ) : (
-          <Redirect to="/admin/login" />
-        )} */}
+          <Redirect to="/login" />
+        )}
       </React.Fragment>
     );
   }
