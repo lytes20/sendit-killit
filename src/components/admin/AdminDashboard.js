a2d92800779275110368613464ee2902a628439b
Daniel Lwetabe <acehanks@gmail.com>
2019-02-28 10:17:21 +0300

    [Feature #164001315] solved CORS error

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import NavBar from './AppBar';
 
 export class AdminDashboard extends Component {
+  state = {
+    orders: []
+  };
   componentDidMount() {
-    //   console.log(this.props);
     fetch('https://francissendit.herokuapp.com/api/v2/parcels', {
-      method: 'POST',
-      credentials: 'include',
+      method: 'GET',
       headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
         token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE1NTEzMTE2MjB9.ArFdUHp2KS6FojNmfLApUIHrTB0iRJA1di7kiGi1WDI',
-        Accept: 'application/json'
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE1NTEzMzgxODV9.AhTb6bZuaCPr9X3v_DuUFUB3MZI5K5Zb-e6gtigZLrU'
       }
     })
       .then(response => response.json())
-      .then(res => {
-        console.log(res);
-      });
+      .then(res => this.setState({ orders: res['All_orders'] }));
   }
   render() {
+    console.log(this.state.orders);
     return (
       <React.Fragment>
         <NavBar />
