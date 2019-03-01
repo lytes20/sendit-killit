2e08e60028bfc8ecc3aa7147ca7f86801bad46e6
Bamuleseyo Gideon <gideonbamuleseyo@gmail.com>
2019-03-01 16:27:58 +0300

    Revert "Ft user login"

--- src/__tests__/actions/signup-tests.js
+++ src/__tests__/actions/signup-tests.js
@@ -1,110 +1,95 @@
-import configureMockStore from 'redux-mock-store';
-import Reduxthunk from 'redux-thunk';
-import * as actions from '../../actions/signup';
-import * as types from '../../actions/types';
+import configureMockStore from "redux-mock-store";
+import Reduxthunk from "redux-thunk";
+import * as actions from "../../actions/signup";
+import * as types from "../../actions/types";
 
 const middlewares = [Reduxthunk];
 const mockStore = configureMockStore(middlewares);
 
-// eslint-disable-next-line no-undef
-describe('Signup actions', () => {
-  // eslint-disable-next-line no-undef
-  it('should create an action to add firstname', () => {
-    const firstname = 'Rogha';
+describe("Signup actions", () => {
+  it("should create an action to add firstname", () => {
+    const firstname = "Rogha";
     const expectedAction = {
       type: types.FIRSTNAME_CHANGED,
       payload: firstname
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onFirstNameChange(firstname)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add lastname', () => {
-    const lastname = 'Timbit';
+  it("should create an action to add lastname", () => {
+    const lastname = "Timbit";
     const expectedAction = {
       type: types.LASTNAME_CHANGED,
       payload: lastname
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onLastNameChange(lastname)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add username', () => {
-    const username = 'EZRogha';
+  it("should create an action to add username", () => {
+    const username = "EZRogha";
     const expectedAction = {
       type: types.USERNAME_CHANGED,
       payload: username
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onUsernameChange(username)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add email', () => {
-    const email = 'rtibi@gmail.com';
+  it("should create an action to add email", () => {
+    const email = "rtibi@gmail.com";
     const expectedAction = {
       type: types.EMAIL_CHANGED,
       payload: email
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onEmailChange(email)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add phone', () => {
-    const phone = '07777777';
+  it("should create an action to add phone", () => {
+    const phone = "07777777";
     const expectedAction = {
       type: types.PHONE_CHANGED,
       payload: phone
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onPhoneChange(phone)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add password', () => {
-    const password = 'rt@12#$';
+  it("should create an action to add password", () => {
+    const password = "rt@12#$";
     const expectedAction = {
       type: types.PASSWORD_CHANGED,
       payload: password
     };
-    // eslint-disable-next-line no-undef
     expect(actions.onPasswordChange(password)).toEqual(expectedAction);
   });
 
-  // eslint-disable-next-line no-undef
-  it('should create an action to add confirm password', () => {
-    // eslint-disable-next-line camelcase
-    const confirm_pass = 'rt@12#$';
+  it("should create an action to add confirm password", () => {
+    const confirm_pass = "rt@12#$";
     const expectedAction = {
       type: types.CONFIRM_PASSWORD_CHANGED,
       payload: confirm_pass
     };
-    // eslint-disable-next-line no-undef
-    expect(actions.onConfirmPasswordChange(confirm_pass)).toEqual(expectedAction);
+    expect(actions.onConfirmPasswordChange(confirm_pass)).toEqual(
+      expectedAction
+    );
   });
 });
 
-// eslint-disable-next-line no-undef
-describe('Register actions', () => {
+describe("Register actions", () => {
   const state = {
-    firstname: '',
-    lastname: '',
-    username: '',
-    email: '',
-    phone: '',
-    password: '',
-    confirm_password: '',
-    error: ''
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirm_password: "",
+    error: ""
   };
-  // eslint-disable-next-line no-undef
-  it('should return error if firstname is invalid', () => {
-    const expectedActions = [{ type: types.ERROR, payload: 'Please provide your firstname' }];
+  it("should return error if firstname is invalid", () => {
+    const expectedActions = [
+      { type: types.ERROR, payload: "Please provide your firstname" }
+    ];
     const store = mockStore({ Signup: state });
     store.dispatch(actions.onRegisterUser(state));
-    // eslint-disable-next-line no-undef
     expect(store.getActions()).toEqual(expectedActions);
   });
 });
--- src/__tests__/components/index-test.js
+++ src/__tests__/components/index-test.js
@@ -2,18 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../components';
 
-// eslint-disable-next-line no-undef
 describe('Index Component', () => {
   let wrapper;
-  // eslint-disable-next-line no-undef
   beforeEach(() => {
-    // eslint-disable-next-line react/jsx-filename-extension
-    wrapper = shallow(<App />);
-  });
-  // eslint-disable-next-line no-undef
+    wrapper = shallow(<App />)
+  })
   it('should render without any error', () => {
-    const div = wrapper.find('div');
-    // eslint-disable-next-line no-undef
-    expect(div.exists()).toBeTruthy();
-  });
-});
+    const div = wrapper.find('div')
+    expect(div.exists()).toBeTruthy()
+  })
+})
