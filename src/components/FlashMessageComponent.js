6b019f0bd04eea7acd874681b5111c0a3d18ce48
anyatibrian <anyatibrian@gmail.com>
2019-03-01 18:20:44 +0300

    [ft-#163906797] edits some files

--- src/components/FlashMessageComponent.js
+++ src/components/FlashMessageComponent.js
@@ -1,17 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import FlashMessage from 'react-flash-message';
+import { connect } from 'react-redux';
 import { removeFlashMessage } from '../actions/orderActions';
-import { connect  } from 'react-redux';
-
 
 class FlashMessageComponent extends React.Component {
 
 
     render() {
-        const { duration, persistOnHover, messageType, messageAlert  } = this.props
+    const { duration, persistOnHover, messageType, messageAlert } = this.props;
 
-        return (messageType === 'success'?
-        (
+    return messageType === 'success' ? (
             <FlashMessage duration={3000} persistOnHover={true}>
                 <div className="alert success_message" role="alert">
                     <p align="center"><strong>{ messageAlert }</strong></p>
@@ -24,8 +24,8 @@ class FlashMessageComponent extends React.Component {
                 <p align="center"><strong>{ messageAlert}</strong></p>
             </div>
         </FlashMessage>
-
         )
+
     }
 
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,14 +6,8 @@ import newOrderReducer from './new_order_reducer';
 import dialogsReducer from './dialogs_reducer';
 
 export default combineReducers({
-<<<<<<< HEAD
-  Signup: SignupReducer,
-  login: LoginReducer
+  login: LoginReducer,
+  newOrder: newOrderReducer,
+  dialogs: dialogsReducer,
+  Signup: SignupReducer
 });
-=======
-    newOrder: newOrderReducer,
-    dialogs: dialogsReducer,
-    Signup: SignupReducer
-
-})
->>>>>>> aa7199b853854e83cf53c8b9287d6138210e8a7f
